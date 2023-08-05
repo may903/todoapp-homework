b097b4e39d94011df4b3fa392205c640225f01c6
may903 <may903@gmail.com>
2023-08-05 16:53:21 +0900

    first commit

---
+++ src/main/java/com/codestates/todoapphomework/TodoappHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.todoapphomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoappHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoappHomeworkApplication.class, args);
+	}
+
+}
